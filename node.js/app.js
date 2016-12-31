1e7ba7c6e0bdff4cfed9e72fb30218b9f2c88693
Shelly <shellybaro@gmail.com>
2016-12-31 16:54:46 +0000

    node struct

---
+++ node.js/app.js
@@ -0,0 +1,15 @@
+var express = require("express");
+
+var app = express();
+
+app.get("/", function(req, res) {
+    res.render("home.ejs");
+});
+
+app.get("/groups", function(req, res) {
+   res.send("Inside the groups"); 
+});
+
+app.listen(process.env.PORT, process.env.IP, function() {
+    console.log("Server has started");
+});
